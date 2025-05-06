5ed876b6d6bf853767ee4a2a22009bcd647311ec
jostinbetun <justin.betun@tecsu.edu.ec>
2025-05-06 16:52:59 -0500

    espero que este bien :)

--- pyqr.py
+++ pyqr.py
@@ -1,9 +1,32 @@
+
 import qrcode
 #pedir al usuario que ingrese 3 valores y almacenarlos en 3 variables diferentes
+# Pedir al usuario que ingrese el primer valor
+valor1 = input("Ingrese el primer valor: ")
 
+# Pedir al usuario que ingrese el segundo valor
+valor2 = input("Ingrese el segundo valor: ")
+
+# Pedir al usuario que ingrese el tercer valor
+valor3 = input("Ingrese el tercer valor: ")
+
+# (Opcional) Convertir los valores a números si son necesarios para operaciones matemáticas
+# valor1 = float(valor1)  # Convierte a número de punto flotante
+# valor2 = float(valor2)
+# valor3 = float(valor3)
+
+# Ahora puedes usar las variables valor1, valor2 y valor3 para trabajar con los valores ingresados
+print("Los valores ingresados son:", valor1, valor2, valor3)
 
 
 #convertir cada variable a entero
+cadena = "123"
+entero = int(cadena)
+print(entero)  # Salida: 123
+
+flotante = 3.14
+entero = int(flotante)
+print(entero)  # Salida: 3
 
 
 
