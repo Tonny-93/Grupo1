6c33b878dbf20146fc7665531adcd8c8a1d99b65
Tonny-93 <tonny.toscano@tecsu.edu.ec>
2025-05-06 15:56:21 -0500

    Create pyqr.py

---
+++ pyqr.py
@@ -0,0 +1,23 @@
+import qrcode
+#pedir al usuario que ingrese 3 valores y almacenarlos en 3 variables diferentes
+
+
+
+#convertir cada variable a entero
+
+
+
+"""
+crear la funcnion operacion 
+esta funcion devolera el resultado de:
+x elevado a la y, dividido para z
+"""
+
+
+resultado = operacion(x,y,z)
+#convertir este resutado en string
+
+#mostrar el resultado en el qr
+img = qrcode.make(resultado)
+type(img)  # qrcode.image.pil.PilImage
+img.save("resultado.png")
