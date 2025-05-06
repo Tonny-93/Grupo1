30b14d524bb4202729d1413096a41019fcd4c75c
Marlyn <marlyn.moreira@tecsu.edu.ec>
2025-05-06 17:05:12 -0500

    exam

--- pyqr.py
+++ pyqr.py
@@ -1,9 +1,10 @@
 
 import qrcode
 #pedir al usuario que ingrese 3 valores y almacenarlos en 3 variables diferentes
-x= "ingrese valor número 1"
+x=(imput"ingrese valor número 1")
 y= "ingrese valor número 2"
-z= "ingrese valor número 3"
+z=(imput"ingrese valor número 3")
+
 
 #convertir cada variable a entero
 
@@ -15,7 +16,7 @@ esta funcion devolera el resultado de:
 x elevado a la y, dividido para z
 ""  
 
-into operacion
+
 
 resultado = operacion(x,y,z)
 #convertir este resutado en string
